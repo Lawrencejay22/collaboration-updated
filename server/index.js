b10e585e987baf2e1336cf66faeffbfe2208e880
lawrence <gabionzalawrencejay@gmail.com>
2026-05-20 12:22:37 +0800

    Fix GitHub OAuth redirect_uri mismatch

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.get('/api/auth/github', (req, res) => {
         `);
     }
 
-    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${CALLBACK_URL}&scope=read:user`;
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=read:user`;
     res.redirect(githubAuthUrl);
 });
 
@@ -57,8 +57,7 @@ app.get('/api/auth/github/callback', async (req, res) => {
         const tokenResponse = await axios.post('https://github.com/login/oauth/access_token', {
             client_id: CLIENT_ID,
             client_secret: CLIENT_SECRET,
-            code: code,
-            redirect_uri: CALLBACK_URL
+            code: code
         }, {
             headers: {
                 Accept: 'application/json'
